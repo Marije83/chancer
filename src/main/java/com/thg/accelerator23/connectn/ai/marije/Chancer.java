11ce3f92a2c9412b87d30f07e351122b8da150c2
marije83 <marije.vanhattum@hotmail.nl>
2024-01-26 15:04:55 +0000

    first attempt at returning random column

--- src/main/java/com/thg/accelerator23/connectn/ai/marije/Chancer.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/marije/Chancer.java
@@ -3,6 +3,9 @@ package com.thg.accelerator23.connectn.ai.marije;
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
 import com.thehutgroup.accelerator.connectn.player.Player;
+import com.thehutgroup.accelerator.connectn.player.Position;
+
+import java.util.Random;
 
 
 public class Chancer extends Player {
@@ -13,8 +16,14 @@ public class Chancer extends Player {
 
   @Override
   public int makeMove(Board board) {
+    Random random = new Random(8);
+    int locationX = random.nextInt();
+
+    while(!board.hasCounterAtPosition(new Position(locationX, 7))) {
+      locationX = random.nextInt();
+    }
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    return 4;
+    return locationX;
   }
 }
